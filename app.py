6b3252bf7a419c8d0336ba319fff5917bf1ff93b
Saif Ansari <saifffansari00@gmail.com>
2025-04-26 20:47:01 +0530

    updated

--- app.py
+++ app.py
@@ -1,16 +1,46 @@
 from flask import Flask, render_template, request
 import joblib
 import string
+from nltk.stem import PorterStemmer
 
 
 model = joblib.load("spam_classifier_model.pkl")
 vectorizer = joblib.load("tfidf_vectorizer.pkl")
 
+stemmer = PorterStemmer()
 
+slang_dict = {
+    "wkly": "weekly",
+    "2": "to",
+    "tkts": "tickets",
+    "freemsg": "free message",
+    "rcv": "receive",
+    "u": "you",
+    "r": "are",
+    "txt": "text",
+    "4": "for",
+    "ur": "your",
+    "mob": "mobile",
+    "ansr": "answer",
+    "msg": "message",
+    "msgs": "messages",
+    "yr": "your",
+    "luv": "love",
+    "plz": "please",
+    "pls": "please",
+}
+
+
+# Updated clean text
 def clean_text(text):
     text = text.lower()
     text = "".join([ch for ch in text if ch not in string.punctuation])
-    return text
+    words = text.split()
+    for index, word in enumerate(words):
+        if word in slang_dict:
+            words[index] = slang_dict[word]
+    filtered = [stemmer.stem(word) for word in words]
+    return " ".join(filtered)
 
 
 def predict_message(message):
