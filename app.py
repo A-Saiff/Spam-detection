ca29cc2ca1542d54c05c9a26b7a39b7af0898042
Saif Ansari <saifffansari00@gmail.com>
2025-04-27 10:38:07 +0530

    used bert transformer instead of tfidf

--- app.py
+++ app.py
@@ -1,56 +1,15 @@
-import spacy
 from flask import Flask, render_template, request
 import joblib
-import string
 from unidecode import unidecode
+from sentence_transformers import SentenceTransformer
 
 model = joblib.load("spam_classifier_model.pkl")
-vectorizer = joblib.load("tfidf_vectorizer.pkl")
-
-
-slang_dict = {
-    "wkly": "weekly",
-    "2": "to",
-    "tkts": "tickets",
-    "freemsg": "free message",
-    "rcv": "receive",
-    "u": "you",
-    "r": "are",
-    "txt": "text",
-    "4": "for",
-    "ur": "your",
-    "mob": "mobile",
-    "ansr": "answer",
-    "msg": "message",
-    "msgs": "messages",
-    "yr": "your",
-    "luv": "love",
-    "plz": "please",
-    "pls": "please",
-    "knw": "know",
-    "askd": "asked"
-}
-nlp = spacy.load("en_core_web_sm", disable=["parser", "ner"])
-
-
-# Updated clean text
-def clean_text(text):
-    text = text.lower()
-    text = "".join([ch for ch in text if ch not in string.punctuation])
-    text = unidecode(text)
-    words = text.split()
-    for index, word in enumerate(words):
-        if word in slang_dict:
-            words[index] = slang_dict[word]
-    cleaned = " ".join(words)
-    doc = nlp(cleaned)
-    lemmas = [token.lemma_.lower().strip() for token in doc]
-    return " ".join(lemmas)
+bert = SentenceTransformer("bert_model")
 
 
 def predict_message(message):
-    cleaned = clean_text(message)
-    vector = vectorizer.transform([cleaned])
+    cleaned = unidecode(message)
+    vector = bert.encode([cleaned])
     prediction = model.predict(vector)[0]
     probability = model.predict_proba(vector)[0]
     return "SPAM" if prediction == 1 else "NOT SPAM", max(probability)
